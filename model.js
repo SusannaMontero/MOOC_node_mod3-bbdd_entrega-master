f3d7a46bfda26471f07000966f0bb2c331e13f9d
SusannaMontero <49920436+SusannaMontero@users.noreply.github.com>
2021-04-26 22:35:06 +0200

    instal moocAuto para corrección

--- model.js
+++ model.js
@@ -1,68 +1,83 @@
-
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
-const options = { logging: false};
+const options = { logging: false };
 const sequelize = new Sequelize("sqlite:db.sqlite", options);
 
 class User extends Model {}
 class Quiz extends Model {}
 
-User.init(
-  { name: {
-      type: DataTypes.STRING,
-      unique: { msg: "Name already exists"},
-      allowNull: false,
-      validate: {
-        isAlphanumeric: { args: true, msg: "name: invalid characters"}
-      }
+User.init({
+    name: {
+        type: DataTypes.STRING,
+        unique: { msg: "Name already exists" },
+        allowNull: false,
+        validate: {
+            isAlphanumeric: { args: true, msg: "name: invalid characters" }
+        }
     },
     age: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isInt: true,
-        min: { args:   [0], msg: "Age: less than 0"},
-        max: { args: [140], msg: "Age: higher than 140"}
-      }
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            isInt: true,
+            min: { args: [0], msg: "Age: less than 0" },
+            max: { args: [140], msg: "Age: higher than 140" }
+        }
     }
-  },
-  { sequelize }
-);
+}, { sequelize });
 
-Quiz.init(
-  { question: {
-      type: DataTypes.STRING,
-      unique: { msg: "Quiz already exists"}
+Quiz.init({
+    question: {
+        type: DataTypes.STRING,
+        unique: { msg: "Quiz already exists" }
     },
     answer: DataTypes.STRING
-  }, 
-  { sequelize }
-);
+}, { sequelize });
 
 
 Quiz.belongsTo(User, {
-  as: 'author', 
-  foreignKey: 'authorId', 
-  onDelete: 'CASCADE'
+    as: 'author',
+    foreignKey: 'authorId',
+    onDelete: 'CASCADE'
 });
 User.hasMany(Quiz, {
-  as: 'posts', 
-  foreignKey: 'authorId'
+    as: 'posts',
+    foreignKey: 'authorId'
 });
 
 // N:N relations default is -> onDelete: 'cascade'
 User.belongsToMany(Quiz, {
-  as: 'fav',
-  foreignKey: 'userId',
-  otherKey: 'quizId',
-  through: 'Favourites'
+    as: 'fav',
+    foreignKey: 'userId',
+    otherKey: 'quizId',
+    through: 'Favourites'
 });
 Quiz.belongsToMany(User, {
-  as: 'fan',
-  foreignKey: 'quizId',
-  otherKey: 'userId',
-  through: 'Favourites'
+    as: 'fan',
+    foreignKey: 'quizId',
+    otherKey: 'userId',
+    through: 'Favourites'
 });
 
-module.exports = sequelize;
+// Se crea
+const { User, Score } = require('./model.js').models;
 
+exports.list = async(rl) => {
+    let scores = await Score.findAll({
+        order: [
+            ['wins', 'DESC']
+        ],
+        include: [{
+            model: User,
+            as: 'player',
+        }, ],
+    });
+
+    scores.forEach((s) =>
+        rl.log(`  ${s.player.name}|${s.wins}|${s.createdAt.toUTCString()}`)
+    );
+};
+
+
+
+module.exports = sequelize;
